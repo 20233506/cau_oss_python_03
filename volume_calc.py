c5cf1685f07ecda83fb1ee6e0364b0934a680d75
miska19090 <129076760+20233506@users.noreply.github.com>
2023-03-27 23:03:11 +0900

    Update volume_calc.py

--- volume_calc.py
+++ volume_calc.py
@@ -1,6 +1,6 @@
-length = input ("가로:12")
-width  = input ("세로:11.5")
-height = input ("높이:7")
+length = input ("가로:")
+width  = input ("세로:")
+height = input ("높이:")
 
 length = float (length)
 width  = float (width)
