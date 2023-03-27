b82aa36cf7daa653ba92928f73f663b9841c20bb
miska19090 <129076760+20233506@users.noreply.github.com>
2023-03-27 22:44:05 +0900

    Create volume_calc.py
    1주차 과제

---
+++ volume_calc.py
@@ -0,0 +1,11 @@
+length = input ("가로:12")
+width  = input ("세로:11.5")
+height = input ("높이:7")
+
+length = float (length)
+width  = float (width)
+height = float (height)
+
+volume = length * width * height
+
+print ("박스의 부피는", volume ,"입니다.")
